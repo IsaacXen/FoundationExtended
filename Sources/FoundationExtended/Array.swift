b322e35faa3aa0978ef2a12a3b927e9acf01aafe
IsaacXen <blackoutxen@gmail.com>
2019-11-08 20:05:30 +0800

    array+dictmap

--- Sources/FoundationExtended/Array.swift
+++ Sources/FoundationExtended/Array.swift
@@ -1,15 +1,15 @@
 import Foundation
 
-public extension Dictionary {
+public extension Array {
     
     /// Returns an array containing the results of calling the given `transform` with each key and value of this dictionary.
     /// - Parameter transform: A closure that accepts an key-value tuple of this dictionary as its argument and returns an tuple value.
-    /// - Parameter pair: The tuple of key and value of this dictionary.
+    /// - Parameter element: The tuple of key and value of this dictionary.
     /// - Returns: An dictionary of the results of calling `transform` with each key-value pair of this dictionary.
-    func dictMap<Key: Hashable, Value>(_ transform: (_ pair: (key: Self.Key, value: Self.Value)) throws -> (Key, Value)) rethrows -> [Key: Value] {
+    func dictMap<Key, Value>(_ transform: (_ element: Element) throws -> (Key, Value)) rethrows -> [Key: Value] where Key: Hashable {
         var dictionary = [Key: Value]()
-        for (k, v) in self {
-            let (key, value) = try transform((k, v))
+        for element in self {
+            let (key, value) = try transform(element)
             dictionary[key] = value
         }
         return dictionary
@@ -17,16 +17,16 @@ public extension Dictionary {
     
     /// Returns an array containing the non-`nil` results of calling the given `transform` with each key and value of this dictionary.
     /// - Parameter transform: A closure that accepts an key-value tuple of this dictionary as its argument and returns an tuple value.
-    /// - Parameter pair: The tuple of key and value of this dictionary.
+    /// - Parameter element: The tuple of key and value of this dictionary.
     /// - Returns: An dictionary of the non-`nil` results of calling `transform` with each key-value pair of this dictionary.
-    func compactDictMap<Key: Hashable, Value>(_ transform: (_ pair: (key: Self.Key, value: Self.Value)) throws -> (Key, Value)?) rethrows -> [Key: Value] {
+    func compactDictMap<Key, Value>(_ transform: (_ element: Element) throws -> (Key, Value)?) rethrows -> [Key: Value] where Key: Hashable {
         var dictionary = [Key: Value]()
-        for (k, v) in self {
-            if let (key, value) = try transform((k, v)) {
+        for element in self {
+            if let (key, value) = try transform(element) {
                 dictionary[key] = value
             }
         }
         return dictionary
     }
-
+    
 }
